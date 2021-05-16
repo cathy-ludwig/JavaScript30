76155c97c0f4612f78aa661f3d5f833d2f42c778
Cathy Ludwig <cathymay9@gmail.com>
2021-05-16 09:00:10 -0500

    day 11 updates

--- 11 - Custom Video Player/scripts.js
+++ 11 - Custom Video Player/scripts.js
@@ -18,12 +18,10 @@ function updateButton() {
 }
 
 function skip() {
-    console.log(this.dataset.skip);
     video.currentTime += parseFloat(this.dataset.skip);
 }
 
 function handleRangeUpdate() {
-    console.log(this.value)
     video[this.name] = this.value;
 }
 
